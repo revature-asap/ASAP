565d02c2f9d193b80eace8c95692cf6c8b9fabf5
Calvin Zheng <calvin.zheng@revature.net>
2021-03-25 14:03:12 -0500

    Implemented UserController Integration Testing

--- src/main/java/com/revature/controllers/UserController.java
+++ src/main/java/com/revature/controllers/UserController.java
@@ -4,11 +4,9 @@ package com.revature.controllers;
 import com.revature.entities.User;
 import com.revature.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -23,6 +21,7 @@ public class UserController {
 
     //Post
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
     void registerUser(@RequestBody User newUser){
         userService.registerUser(newUser);
     }
