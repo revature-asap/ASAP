9eb1a498bc09963ad217d3a81091e59dc1caaa1a
Brandon Johnson <brandon.johnson@revature.net>
2021-03-25 11:49:19 -0500

    logging aspect implemented-- picks up on driver, and logs to console  -- will test more, when there is more backend to test

--- src/main/java/com/revature/ASAPDriver.java
+++ src/main/java/com/revature/ASAPDriver.java
@@ -1,5 +1,6 @@
 package com.revature;
 
+import com.revature.entities.Asset;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
