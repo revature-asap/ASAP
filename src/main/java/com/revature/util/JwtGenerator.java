a4f8c5a9c03e326fb28f3a0f71c53670f10e5c3c
unknown <alex.googe@revature.net>
2021-03-26 17:09:48 -0400

    Created a configuration class for JWT and a generator for JWTs.

---
+++ src/main/java/com/revature/util/JwtGenerator.java
@@ -0,0 +1,39 @@
+package com.revature.util;
+
+import dtos.Principal;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtGenerator {
+
+    private JwtConfig config;
+
+    @Autowired
+    public JwtGenerator(JwtConfig config) {
+        this.config = config;
+    }
+
+    public String generateJwt (Principal subject) {
+
+        long now = System.currentTimeMillis();
+
+        JwtBuilder builder = Jwts.builder()
+                .setId(String.valueOf(subject.getId()))
+                .setSubject(subject.getUsername())
+                .claim("role", subject.getRole())
+                .setIssuer("revature")
+                .setIssuedAt(new Date(now))
+                .setExpiration(new Date(now + (60 * 60 * 1000)))
+                .signWith(config.getSignatureAlgorithm(), config.getSigningKey());
+
+        return builder.compact();
+
+
+    }
+
+}
