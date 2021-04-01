607086e3fc1aa31a9c30db4dc3d6dc482017730c
Brandon Johnson <brandon.johnson@revature.net>
2021-04-01 13:36:42 -0500

    basic post pojo

--- src/main/java/com/revature/entities/Post.java
+++ src/main/java/com/revature/entities/Post.java
@@ -1,4 +1,44 @@
 package com.revature.entities;
 
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Table(name="posts")
 public class Post {
+
+//    post_id serial,
+//    author_id int,
+//    asset_id int,
+//    title varchar(256) not null,
+//    text_content text not null,
+//    image_content bytea,
+//    creation_timestamp timestamp not null default now(),
+//    parent_post_id int,
+    @Id()
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name="author_id")
+    private String authorId;
+
+    @Column(name="asset_id")
+    private String assetId;
+
+    @Column(name="title", nullable = false)
+    private String title;
+
+    @Column(name="text_content", nullable = false)
+    private String textContent;
+
+    @Column(name="image_content") //TODO <-- NOT A STRING
+    private String imageContent;
+
+    @Column(name="creation_timestamp", nullable = false)
+    private Date creationTimestamp;
+
+    @Column(name="parent_post_id", nullable = false)
+    private int parentPostId;
+
+
 }
