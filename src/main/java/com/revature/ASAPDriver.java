d15aaa1bef0aa01c84f740eb89aefbb80f6e2aac
Nate Gamble <nate.eli.gamble@gmail.com>
2021-04-02 11:13:58 -0400

    corrected swagger config, added error handling aspect

--- src/main/java/com/revature/ASAPDriver.java
+++ src/main/java/com/revature/ASAPDriver.java
@@ -26,7 +26,7 @@ public class ASAPDriver {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.revature.web.controllers"))
+                .apis(RequestHandlerSelectors.basePackage("com.revature.controllers"))
                 .paths(PathSelectors.any())
                 .build();
     }
