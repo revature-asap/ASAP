5696757e682dcf82a55ceb01bbcb622bfde643b5
Brandon Johnson <brandon.johnson@revature.net>
2021-04-05 15:07:56 -0500

    documentation for authorization and authentication exceptions

--- src/main/java/com/revature/exceptions/AuthenticationException.java
+++ src/main/java/com/revature/exceptions/AuthenticationException.java
@@ -1,12 +1,12 @@
 package com.revature.exceptions;
 
 /**
- * An exception that gets called when a user attempts to access an unauthorized endpoint
+ * An exception that gets called when a user attempts to access an endpoint that requires a user be logged in
  */
 public class AuthenticationException extends RuntimeException {
 
     public AuthenticationException() {
-        super("Authentification Failed!");
+        super("Authentication Failed! Please log in before trying to proceed.");
     }
 
     public AuthenticationException(String message) {
