44db6fac54114e71c9d2daef43d5dda6c5501983
Nate Gamble <nate.eli.gamble@gmail.com>
2021-04-05 15:39:07 -0400

    changed how exceptions were being thrown and handled. Logic now flows properly from database to finnhub to lunarcrush to 404

--- src/main/java/com/revature/services/FinnhubService.java
+++ src/main/java/com/revature/services/FinnhubService.java
@@ -7,8 +7,10 @@ import com.revature.repositories.AssetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import java.time.LocalDate;
+import java.util.NoSuchElementException;
 
 @Service
 public class FinnhubService {
@@ -23,55 +25,61 @@ public class FinnhubService {
         //THIS is where we do logic to see if the asset is in the repository/database - otherwise we push the call off to the finnhub service
         public Asset getAsset(String ticker) {
             ticker = ticker.toUpperCase();
-            System.out.println("RETURN FROM FINDBYTICKER ON REPO: " + assetRepo.findAssetByTicker(ticker));
-            // If ticker is in database
-            if(assetRepo.findAssetByTicker(ticker).isPresent()){
-                Asset assetToCheck = assetRepo.findAssetByTicker(ticker).get();
-                // If data is old and needs to be refreshed
-                if (assetToCheck.getLastTouchedTimestamp().isBefore(LocalDate.now().minusDays(1))) {
+            // System.out.println("RETURN FROM FINDBYTICKER ON REPO: " + assetRepo.findAssetByTicker(ticker));
+            
+            try {
+                // If ticker is in database
+                Asset savedAsset = assetRepo.findAssetByTicker(ticker).get();
+                if (savedAsset.getLastTouchedTimestamp().isBefore(LocalDate.now().minusDays(1))) {
                     //OLD - get record from finnhub and update database entry
                     return retrieveAssetFromApi(ticker);
                 } else {
                     //STILL VALID - return asset
-                    System.out.println("SAVED CALL TO FINNHUB FOR ASSET: " + assetToCheck);
-                    return assetToCheck;
+                    System.out.println("SAVED CALL TO FINNHUB FOR ASSET: " + savedAsset);
+                    return savedAsset;
                 }
-            } else {
-                // If ticker is not in database
+            } catch (NoSuchElementException e) {
                 return retrieveAssetFromApi(ticker);
             }
         }
 
         public Asset retrieveAssetFromApi(String ticker) {
+            //check database to actually get that asset if it exists
+            Asset dbAsset;
+            try {
+                dbAsset = assetRepo.findAssetByTicker(ticker).get();
+            } catch (NoSuchElementException e) {
+                dbAsset = null;
+            }
+
             // Search finnhub first (for stock tickers)
             Asset assetToCheck = searchFinnhub(ticker);
-            //check database to actually get that asset
-            Asset dbAsset = assetRepo.findAssetByTicker(ticker).get();
-            if (assetToCheck.getName() != null) {
-                if (assetToCheck.getName().equals(dbAsset.getName())) {
+            // if we got an Asset back from Finnhub
+            if (assetToCheck != null && assetToCheck.getName() != null) {
+                if (dbAsset != null && assetToCheck.getName().equals(dbAsset.getName())) {
                     //we are updating a record in the database here
                     dbAsset.setLastTouchedTimestamp(LocalDate.now());
-                    assetRepo.save(dbAsset); //!
+                    assetRepo.save(dbAsset);
                     return dbAsset;
                 } else {
                     //we are creating a new record in the database here
                     assetToCheck.setLastTouchedTimestamp(LocalDate.now());
-                    assetRepo.save(assetToCheck); //!
+                    assetRepo.save(assetToCheck);
                     return assetToCheck;
                 }
             } else {
                 // Search lunarcrush next (for crypto currencies)
                 assetToCheck = searchLunarCrush(ticker);
-                if (assetToCheck.getName() != null) {
-                    if (assetToCheck.getName().equals(dbAsset.getName())) {
+                if (assetToCheck != null && assetToCheck.getName() != null) {
+                    if (dbAsset != null && assetToCheck.getName().equals(dbAsset.getName())) {
                         //we are updating a record in the database here
                         dbAsset.setLastTouchedTimestamp(LocalDate.now());
-                        assetRepo.save(dbAsset); //!
+                        assetRepo.save(dbAsset);
                         return dbAsset;
                     } else {
                         //we are creating a new record in the database here
                         assetToCheck.setLastTouchedTimestamp(LocalDate.now());
-                        assetRepo.save(assetToCheck); //!
+                        assetRepo.save(assetToCheck);
                         return assetToCheck;
                     }
                 } else {
@@ -80,6 +88,12 @@ public class FinnhubService {
             }
         }
         
+        /**
+         * Searches Finnhub to retrieve a stock for a ticker.
+         * If the ticker does not exist on the Finnhub API, returns {@code null}
+         * @param ticker
+         * @return an {@code Asset} corresponding to {@code ticker} or {@code null}
+         */
         public Asset searchFinnhub(String ticker) {
             WebClient client;
             client = WebClient.create("https://finnhub.io/api/v1/stock/profile2?token=c1ceppv48v6scqmqtk5g&symbol="+ ticker);
@@ -87,19 +101,30 @@ public class FinnhubService {
                     .uri(uriBuilder -> uriBuilder.build())
                     .retrieve()
                     .bodyToMono(Asset.class)//map results to a Asset
-                    .blockOptional().orElseThrow(RuntimeException::new);
+                    .blockOptional().orElse(null);
         }
-
-        // Doesn't return an actual Asset right now, may need to parse the return from lunarcrush a bit
+        
+        /**
+         * Searches LunarCrush to retrieve a cryptocurrency for a ticker.
+         * If the ticker does not exist on the LunarCrush API, returns {@code null}
+         * @param ticker
+         * @return an {@code Asset} corresponding to {@code ticker} or {@code null}
+         */
         public Asset searchLunarCrush(String ticker) {
             WebClient client;
-            client = WebClient.create("https://api.lunarcrush.com/v2?data=assets&key=x9aazwqfpgvfd08gtrd2&symbol="+ ticker);
-            LunarCrushDTO cryptoAsset = client.get()
-                    .uri(uriBuilder -> uriBuilder.build())
-                    .retrieve()
-                    .bodyToMono(LunarCrushDTO.class) //map results to a LunarCrushDTO
-                    .blockOptional().orElseThrow(RuntimeException::new);
-            return cryptoAsset.getAsset(); // convert the LunarCrush object to an Asset
+            try {
+                client = WebClient.create("https://api.lunarcrush.com/v2?data=assets&key=x9aazwqfpgvfd08gtrd2&symbol="+ ticker);
+                LunarCrushDTO cryptoAsset = client.get()
+                        .uri(uriBuilder -> uriBuilder.build())
+                        .retrieve()
+                        .bodyToMono(LunarCrushDTO.class) //map results to a LunarCrushDTO
+                        .blockOptional().orElse(null);
+                return cryptoAsset.getAsset(); // convert the LunarCrush object to an Asset
+            } catch (WebClientResponseException e) {
+                // LunarCrush will return a 502 if the ticker symbol doesn't exist in their database
+                // so we want to catch it and return null here
+                return null;
+            }
         }
 
 
