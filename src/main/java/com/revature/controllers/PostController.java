807cc8a43591b534a1db640ef04be8800c916e29
Nate Gamble <nate.eli.gamble@gmail.com>
2021-04-05 14:55:44 -0400

    removed unnecessary code setting http status codes  and unused imports

--- src/main/java/com/revature/controllers/PostController.java
+++ src/main/java/com/revature/controllers/PostController.java
@@ -1,15 +1,8 @@
 package com.revature.controllers;
 
 
-import com.revature.annotations.Secured;
-import com.revature.dtos.Principal;
 import com.revature.entities.Post;
-import com.revature.entities.User;
-import com.revature.entities.UserRole;
 import com.revature.services.PostService;
-import com.revature.services.UserService;
-import com.revature.util.JwtGenerator;
-import com.revature.util.JwtParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -26,8 +19,6 @@ import java.util.List;
 @RequestMapping("/posts")
 public class PostController {
 
-    private final String WEB_URL = "http://p3-210119-java-enterprise.s3-website.us-east-2.amazonaws.com/";
-    private final String APP_URL = "http://localhost:5000";
     private final PostService postService;
 
 
@@ -49,17 +40,15 @@ public class PostController {
      */
 //    @Secured(allowedRoles = "ADMIN")
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Post> getAllPosts(HttpServletRequest request, HttpServletResponse response){
+    public List<Post> getAllPosts(){
 
-        response.setStatus(200);
         return postService.getAllPosts();
     }
 
     //    @Secured(allowedRoles = "ADMIN")
     @GetMapping(path="/{parentPostId}", produces = MediaType.APPLICATION_JSON_VALUE)
-        public List<Post> getPostsByParentId(@PathVariable int parentPostId, HttpServletRequest request, HttpServletResponse response){
+    public List<Post> getPostsByParentId(@PathVariable int parentPostId){
 
-        response.setStatus(200);
         return postService.getPostsByParentPostId(parentPostId);
     }
 
@@ -72,9 +61,8 @@ public class PostController {
      */
 //    @Secured(allowedRoles = {"ADMIN","BASIC"})
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void makePost(@RequestBody Post newPost, HttpServletRequest request, HttpServletResponse response){
+    public void makePost(@RequestBody Post newPost){
 
-        response.setStatus(200);
         postService.makePost(newPost);
 
     }
@@ -88,9 +76,8 @@ public class PostController {
      */
 //    @Secured(allowedRoles = {"ADMIN","BASIC"})
     @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void editPost(@RequestBody Post newPost, HttpServletRequest request, HttpServletResponse response){
+    public void editPost(@RequestBody Post newPost){
 
-        response.setStatus(200);
         postService.editPost(newPost);
 
     }
@@ -104,11 +91,9 @@ public class PostController {
      */
 //    @Secured(allowedRoles = "ADMIN")
     @DeleteMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void deletePost(@RequestBody Post post, HttpServletRequest request, HttpServletResponse response){
-
-        response.setStatus(200);
+    public void deletePost(@RequestBody Post post){
+        
         postService.deletePost(post);
-
     }
 
 }
