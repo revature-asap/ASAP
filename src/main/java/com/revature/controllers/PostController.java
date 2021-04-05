d2fa36735b98798cb30ea3e3c3744b6487bd1114
Brandon Johnson <brandon.johnson@revature.net>
2021-04-05 13:27:05 -0500

    Implemented getPostsByParentPostId endpoing, javadocs for a few classes, cleaned up some code

--- src/main/java/com/revature/controllers/PostController.java
+++ src/main/java/com/revature/controllers/PostController.java
@@ -18,6 +18,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
+/**
+ * Post controller:
+ *  endpoints for making, viewing, altering and removing posts/comments
+ */
 @RestController
 @RequestMapping("/posts")
 public class PostController {
@@ -25,26 +29,47 @@ public class PostController {
     private final String WEB_URL = "http://p3-210119-java-enterprise.s3-website.us-east-2.amazonaws.com/";
     private final String APP_URL = "http://localhost:5000";
     private final PostService postService;
-    private final JwtGenerator jwtGenerator;
-    private JwtParser jwtparser;
 
 
+    /**
+     * Constructor for autowiring the postService
+     * @param postService service class for posts
+     */
     @Autowired
-    public PostController(PostService postService, JwtGenerator jwtGenerator, JwtParser jwtparser){
+    public PostController(PostService postService){
         this.postService = postService;
-        this.jwtGenerator = jwtGenerator;
-        this.jwtparser = jwtparser;
     }
 
 
+    /**
+     * gets all posts from the database, along with related information such as poster and asset associated with it
+     * @param request is the Http Servlet Request
+     * @param response is the Http Servlet Response
+     * @return all users in the database
+     */
 //    @Secured(allowedRoles = "ADMIN")
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Post> getAllPosts(HttpServletRequest request, HttpServletResponse response){
 
         response.setStatus(200);
         return postService.getAllPosts();
-
     }
+
+    //    @Secured(allowedRoles = "ADMIN")
+    @GetMapping(path="/{parentPostId}", produces = MediaType.APPLICATION_JSON_VALUE)
+        public List<Post> getPostsByParentId(@PathVariable int parentPostId, HttpServletRequest request, HttpServletResponse response){
+
+        response.setStatus(200);
+        return postService.getPostsByParentPostId(parentPostId);
+    }
+
+    /**
+     * Inserts a new post/comment into the database.
+     * requires json body containing: id(==0), authorId, assetId, title, and textContent
+     * @param newPost the json body of the clients request
+     * @param request
+     * @param response responds with status code
+     */
 //    @Secured(allowedRoles = {"ADMIN","BASIC"})
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public void makePost(@RequestBody Post newPost, HttpServletRequest request, HttpServletResponse response){
@@ -53,6 +78,14 @@ public class PostController {
         postService.makePost(newPost);
 
     }
+
+    /**
+     * edits a post based on id.
+     * requires json containing the id of the post to edit, the new title and new textContent
+     * @param newPost the json body of the clients request
+     * @param request
+     * @param response responds with status code
+     */
 //    @Secured(allowedRoles = {"ADMIN","BASIC"})
     @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public void editPost(@RequestBody Post newPost, HttpServletRequest request, HttpServletResponse response){
@@ -62,6 +95,13 @@ public class PostController {
 
     }
 
+    /**
+     * Deletes a post/comment from the database.
+     * requires admin and json body containing id of post to remove
+     * @param post the json body of the clients request
+     * @param request
+     * @param response responds with status code
+     */
 //    @Secured(allowedRoles = "ADMIN")
     @DeleteMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public void deletePost(@RequestBody Post post, HttpServletRequest request, HttpServletResponse response){
