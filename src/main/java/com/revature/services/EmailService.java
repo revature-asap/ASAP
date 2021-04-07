6c486ab9b4cb97a20cb9848acaf1e39a0ba76cd8
Nate Gamble <nate.eli.gamble@gmail.com>
2021-04-07 17:05:20 -0400

    removed unused imports

--- src/main/java/com/revature/services/EmailService.java
+++ src/main/java/com/revature/services/EmailService.java
@@ -1,7 +1,6 @@
 package com.revature.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
